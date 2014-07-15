eacd1ff9c322fe73d58d5d292357a91689de1a06
Ahmed ElSayed <ahmels@microsoft.com>
2014-07-14 19:15:55 -0700

    fix up setProfileEnvironment in KuduDevSetup.cmd, fixes #674

--- Setup/KuduDevSetup.cmd
+++ Setup/KuduDevSetup.cmd
@@ -4,3 +4,4 @@ set webpicmd="%ProgramFiles%\Microsoft\Web Platform Installer\WebPiCmd.exe"
 set customFeed="%~dp0KuduSetupCustomWebPiFeed.xml"
 set logFile="%~dp0WebPiCmdSetup.log"
 %webpicmd% /Install  /SuppressReboot /Products:KuduDevSetup /Feeds:%customFeed% /Log:%logFile%
+%systemroot%\system32\inetsrv\APPCMD set config -section:system.applicationHost/applicationPools /applicationPoolDefaults.processModel.setProfileEnvironment:"true" /commit:apphost
